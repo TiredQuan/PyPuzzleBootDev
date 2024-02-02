b772c2d02001e5784b0f3379886cd7d78538d5ae
TiredQuan <quanproductioncentre@gmail.com>
2024-02-02 16:19:48 +0700

    added todo commends

--- test.py
+++ test.py
@@ -4,13 +4,18 @@ class Window:
 	def __init__(self,width,height):
 		self.__root = Tk()
 		self.__root.title('lmalmao')
-		self.__canvas = Canvas(root, bg='#000000',width=width,height=height)
+		self.__canvas = Canvas(self.__root, bg='#000000',width=width,height=height)
 		self.__canvas.pack()
 
 
-root = Tk()
-root.title('lmalmao')
-canvas = Canvas(root, bg='#000000',width=800,height=800)
-canvas.pack()
+# root = Tk()
+# root.title('lmalmao')
+# canvas = Canvas(root, bg='#000000',width=800,height=800)
+# canvas.pack()
 
-root.mainloop()
+# root.mainloop()
+
+window = Window(500,500)
+# TODO
+# ask
+# What's the difference between mainloop() and class update/update_idletasks()
